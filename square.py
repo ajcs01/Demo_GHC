579b7362557003537722d9d375a6ddbe13097bb5
Ali Jaddoa <a.h.jaddoa@gmail.com>
2025-09-18 14:09:02 +0100

    init

---
+++ square.py
@@ -0,0 +1,9 @@
+def squaring_num():
+    
+
+    ''' Your solution goes here '''
+    
+    average=??
+    
+    #return the final average of the normalised values of the given inputs without fail
+    return average
---
+++ square_test.py
@@ -0,0 +1,5 @@
+import square
+
+def test_square():
+    assert square.squaring_num() == 0.51
+
